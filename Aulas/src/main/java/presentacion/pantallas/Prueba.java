3961b2180c575e81f99b261d86ca6254d59a477c
luisa <moraLF20@outlook.com>
2024-04-05 22:46:22 -0600

    probando un metodo para los diasEvento

--- Aulas/src/main/java/presentacion/pantallas/Prueba.java
+++ Aulas/src/main/java/presentacion/pantallas/Prueba.java
@@ -3,6 +3,8 @@ package presentacion.pantallas;
 
 import DTOS.maestro.MaestroEditableDTO;
 import excepciones.NegocioException;
+import java.util.ArrayList;
+import java.util.List;
 import subsistemas.recuperarMaestro.IRecuperarMaestro;
 import subsistemas.recuperarMaestro.FachadaRecuperarMaestro;
 
@@ -18,11 +20,32 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+//        String diasEvento="Lu,Mi,Vi";
+//        String[] diasArr=diasEvento.split(",",7);
+//            List<Integer> diasNum=new ArrayList<>();
+//            for(String s:diasArr){
+//                switch(s){
+//                    case "Do" -> diasNum.add(1);
+//                    case "Lu" -> diasNum.add(2);
+//                    case "Ma" -> diasNum.add(3);
+//                    case "Mi" -> diasNum.add(4);
+//                    case "Ju" -> diasNum.add(5);
+//                    case "Vi" -> diasNum.add(6);
+//                    case "Sa" -> diasNum.add(7);
+//                }
+//            }
+//        for(Integer i:diasNum){
+//            System.out.println(i);
+//        }
         IRecuperarMaestro rMaestro = new FachadaRecuperarMaestro();
-        MaestroEditableDTO maestroE = null;
+        MaestroEditableDTO m;
+        MaestroEditableDTO maestroE=null;
+        
+        
         try {
-            maestroE = rMaestro.recuperarMaestro();
+            m = rMaestro.recuperarMaestro();    
+            maestroE = rMaestro.recuperarMaestro2(m);
+            System.out.println(maestroE.getNombre());
         } catch (NegocioException e) {
             System.out.println("error al cargar el maestro");
         }
