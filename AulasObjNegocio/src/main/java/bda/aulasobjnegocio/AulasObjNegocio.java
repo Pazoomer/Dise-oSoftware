2ff52e49c719c6763d9f7c39ec42bd944f0945ff
luisa <moraLF20@outlook.com>
2024-04-14 21:55:32 -0700

    main de prueba de obj negocio

--- AulasObjNegocio/src/main/java/bda/aulasobjnegocio/AulasObjNegocio.java
+++ AulasObjNegocio/src/main/java/bda/aulasobjnegocio/AulasObjNegocio.java
@@ -4,6 +4,19 @@
 
 package bda.aulasobjnegocio;
 
+import DTOS.campus.UbicacionDTO;
+import DTOS.evento.EventoConsultableDTO;
+import DTOS.evento.TipoEventoEnumDTO;
+import DTOS.maestro.MaestroEditableDTO;
+import entidades.ClaseConexion;
+import excepciones.NegocioException;
+import excepcioness.PersistenciaExceptionn;
+import java.util.Calendar;
+import java.util.List;
+import objetosNegocio.Evento;
+import objetosNegocio.Maestro;
+import objetosNegocio.Ubicacion;
+
 /**
  *
  * @author luiis
@@ -11,6 +24,51 @@ package bda.aulasobjnegocio;
 public class AulasObjNegocio {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Maestro m=new Maestro();
+        Evento ev=new Evento();
+        Ubicacion ubi=new Ubicacion();
+        try{
+            List<UbicacionDTO> ubicaciones=ubi.obtenerUbicaciones();
+            if(ubicaciones!=null && !ubicaciones.isEmpty()){
+                for(UbicacionDTO u: ubicaciones){
+                    System.out.println(u.getIdentificador());
+                }
+            }System.out.println("no se obtuvieron");
+//            MaestroEditableDTO maestro=m.obtenerMaestro(new MaestroEditableDTO(1L));
+//            System.out.println(maestro.toString());
+//////            maestro.setDescripcion("Doy asesorias de 9 a 11 de bases de datos los sabados y domingos");
+//////            MaestroEditableDTO maestroEditado=m.editarMaestro(maestro);
+//            EventoConsultableDTO evento=new EventoConsultableDTO("clase de diseño");
+////            
+//            Calendar fechaInicio=Calendar.getInstance();
+//            Calendar horaInicio=Calendar.getInstance();
+//            horaInicio.set(Calendar.HOUR_OF_DAY, 10);
+//            horaInicio.set(Calendar.MINUTE, 30);
+//            fechaInicio.set(Calendar.MONTH, 0);
+//            fechaInicio.set(Calendar.DAY_OF_MONTH, 15);
+//            Calendar fechaFin=fechaInicio;
+//            fechaFin.add(Calendar.MONTH, 4);
+//            UbicacionDTO ubicacion=ubi.obtenerUbicacion(new UbicacionDTO("AV1800"));
+//            System.out.println(ubicacion.toString());
+//            System.out.println(ubicacion.getId());
+//            EventoConsultableDTO eventoNuevo=new EventoConsultableDTO(TipoEventoEnumDTO.SEMANAL,"clase de diseño",
+//                    "nueva clase diseño", "yellow","0101000",ubicacion ,
+//                    fechaInicio,fechaFin ,horaInicio,1.5f);
+//            eventoNuevo.setMaestro(maestro);
+//            if(ev.agregarEvento(eventoNuevo)){
+//                System.out.println("se agrego");
+//            }else System.out.println("no se agrego");
+//            evento.setTipo(TipoEventoEnumDTO.SEMANAL);
+//            EventoConsultableDTO eventoObtenido=ev.obtenerEvento(evento);
+//            System.out.println(eventoObtenido.toString());
+//            System.out.println(maestroEditado.toString());
+        }catch(NegocioException e){
+            System.out.println(e.getMessage());
+        }
+        try{
+            ClaseConexion.cerrarConexion();
+        }catch(PersistenciaExceptionn e){
+            System.out.println(e.getMessage());
+        }
     }
 }
