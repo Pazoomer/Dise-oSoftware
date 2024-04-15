5f0fc6298f2b1cc4c4dbe6f7d28ce51fec1968dc
luisa <moraLF20@outlook.com>
2024-04-14 21:58:06 -0700

    modificando las conversiones

--- AulasObjNegocio/src/main/java/objetosNegocio/Conversiones.java
+++ AulasObjNegocio/src/main/java/objetosNegocio/Conversiones.java
@@ -4,15 +4,16 @@
  */
 package objetosNegocio;
 
+import DTOS.campus.CampusConsultableDTO;
+import DTOS.campus.UbicacionDTO;
 import DTOS.evento.EventoConsultableDTO;
 import DTOS.evento.TipoEventoEnumDTO;
 import DTOS.maestro.MaestroEditableDTO;
+import entidades.EntidadCampus;
 import entidades.EntidadEvento;
 import entidades.EntidadMaestro;
 import entidades.EntidadTipoEventoEnum;
-import static entidades.EntidadTipoEventoEnum.SEMANAL;
-import static entidades.EntidadTipoEventoEnum.UNICO_UN_DIA;
-import static entidades.EntidadTipoEventoEnum.UNICO_VARIOS_DIAS;
+import entidades.EntidadUbicacion;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,96 +23,154 @@ import java.util.List;
  */
 class Conversiones {
     
+    protected EntidadUbicacion toUbicacionBO(UbicacionDTO ubicacionDTO){
+        EntidadUbicacion ubicacion;
+        if(ubicacionDTO.getCampus()!=null){
+            ubicacion=new EntidadUbicacion(
+                ubicacionDTO.getIdentificador(),
+                new EntidadCampus(ubicacionDTO.getCampus().getNombre()),
+                ubicacionDTO.getDescripcion());
+        }else{
+            ubicacion=new EntidadUbicacion(ubicacionDTO.getIdentificador());
+        }
+        ubicacion.setId(ubicacionDTO.getId());
+        return ubicacion;
+    }
+    
+    protected UbicacionDTO toUbicacionDTO(EntidadUbicacion ubicacionBO){
+        UbicacionDTO ubicacion;
+        if(ubicacionBO.getCampus()!=null){
+            ubicacion=new UbicacionDTO(
+                    ubicacionBO.getIdentificador(),
+                    ubicacionBO.getDescripcion(),
+                    new CampusConsultableDTO(ubicacionBO.getCampus().getNombre())
+            );
+        }else{
+            ubicacion=new UbicacionDTO(ubicacionBO.getIdentificador());
+        }
+        ubicacion.setId(ubicacionBO.getId());
+        return ubicacion;
+    }
+    
     protected EntidadMaestro toMaestroBO(MaestroEditableDTO maestro) {
         List<EventoConsultableDTO> eventos = maestro.getCalendario();
         List<EntidadEvento> eventosBO = new ArrayList<>();
-        if (!eventos.isEmpty()) {
+        EntidadUbicacion ubicacionBO=null;
+        EntidadMaestro maestroBO;
+        if(maestro.getCubiculo()!=null)
+            ubicacionBO=toUbicacionBO(maestro.getCubiculo());
+        if (eventos != null && !eventos.isEmpty()) {
             for (EventoConsultableDTO ec : eventos) {
                 eventosBO.add(toEventoBO(ec));
             }
+            maestroBO= new EntidadMaestro(
+                    maestro.getId(),
+                    maestro.getNombre(),
+                    ubicacionBO,
+                    maestro.getDescripcion(),
+                    maestro.getFoto(),
+                    eventosBO
+            );
+        }else{
+            maestroBO= new EntidadMaestro(
+                    maestro.getId(),
+                    maestro.getNombre(),
+                    ubicacionBO,
+                    maestro.getDescripcion(),
+                    maestro.getFoto()
+            );
         }
-        EntidadMaestro maestroConvertido = new EntidadMaestro(
-                1L,
-                maestro.getNombre(),
-                maestro.getCubiculo(),
-                maestro.getDescripcion(),
-                maestro.getFoto(),
-                eventosBO
-        );
-        return maestroConvertido;
+        maestroBO.setIdBd(maestro.getIdBD());
+        return maestroBO;
     }
 
     protected MaestroEditableDTO toMaestroDTO(EntidadMaestro maestro) {
         List<EntidadEvento> eventos = maestro.getCalendario();
         List<EventoConsultableDTO> eventosDTO = new ArrayList<>();
-        if (!eventos.isEmpty()) {
-            for (EntidadEvento ec : eventos) {
-                eventosDTO.add(toEventoDTO(ec)
-                );
-            }
-        }
         MaestroEditableDTO maestroDTO = new MaestroEditableDTO(
+                maestro.getIdMaestro(),
                 maestro.getNombre(),
-                maestro.getCubiculo(),
+                toUbicacionDTO(maestro.getCubiculo()),
                 maestro.getDescripcion(),
-                maestro.getFoto(),
-                eventosDTO
+                maestro.getFoto()
         );
+        maestroDTO.setIdBD(maestro.getIdBd());
+        if (eventos != null && !eventos.isEmpty()) {
+            for (EntidadEvento ec : eventos) {
+                eventosDTO.add(toEventoDTO(ec,maestroDTO));
+            }
+            maestroDTO.setCalendario(eventosDTO);
+        }
         return maestroDTO;
     }
     
+    
     protected EntidadEvento toEventoBO(EventoConsultableDTO evento) {
-        EntidadEvento eventoConvertido = null;
+        EntidadEvento eventoConvertido=null;
+        EntidadUbicacion ubi=null;
+        EntidadMaestro maestroBO=null;
+        if(evento.getMaestro()!=null) maestroBO=toMaestroBO(evento.getMaestro());
+        if(evento.getUbicacion()!=null)
+            ubi=toUbicacionBO(evento.getUbicacion());
         switch (evento.getTipo()) {
             case UNICO_UN_DIA ->
                 eventoConvertido = new EntidadEvento(
-                        evento.getColor(),
                         evento.getNombre(),
+                        evento.getColor(),
+                        ubi,
                         evento.getDescripcion(),
                         evento.getFechaInicio(),
-                        evento.getUbicacion(),
                         evento.getHoraInicio(),
-                        evento.getHorasDuracionEvento()
+                        evento.getHorasDuracionEvento(),
+                        maestroBO
                 );
             case UNICO_VARIOS_DIAS ->
                 eventoConvertido = new EntidadEvento(
-                        evento.getColor(),
                         EntidadTipoEventoEnum.UNICO_VARIOS_DIAS,
                         evento.getNombre(),
                         evento.getDescripcion(),
                         evento.getDiasSemana(),
-                        evento.getUbicacion(),
+                        ubi,
+                        evento.getColor(),
                         evento.getFechaInicio(),
                         evento.getFechaFin(),
                         evento.getHoraInicio(),
-                        evento.getHorasDuracionEvento()
+                        evento.getHorasDuracionEvento(),
+                        maestroBO
                 );
             case SEMANAL ->
                 eventoConvertido = new EntidadEvento(
-                        evento.getColor(),
                         EntidadTipoEventoEnum.SEMANAL,
                         evento.getNombre(),
                         evento.getDescripcion(),
                         evento.getDiasSemana(),
-                        evento.getUbicacion(),
+                        ubi,
+                        evento.getColor(),
                         evento.getFechaInicio(),
                         evento.getFechaFin(),
                         evento.getHoraInicio(),
-                        evento.getHorasDuracionEvento()
+                        evento.getHorasDuracionEvento(),
+                        maestroBO
                 );
         }
+        if(eventoConvertido!=null)eventoConvertido.setId(evento.getId());
         return eventoConvertido;
     }
     
-    protected EventoConsultableDTO toEventoDTO(EntidadEvento evento) {
+    protected EventoConsultableDTO toEventoDTO(EntidadEvento evento,MaestroEditableDTO maestroDTO) {
         EventoConsultableDTO eventoConvertido = null;
+        UbicacionDTO ubicacionDTO=null;
+        MaestroEditableDTO maestroDTO2=null;
+        if(evento.getUbicacion()!=null) ubicacionDTO=toUbicacionDTO(evento.getUbicacion());
+        if(maestroDTO!=null)maestroDTO2=maestroDTO;
+        else if(evento.getMaestro()!=null)maestroDTO2=toMaestroDTO(evento.getMaestro());
         switch (evento.getTipo()) {
             case UNICO_UN_DIA ->
                 eventoConvertido = new EventoConsultableDTO(
                         evento.getNombre(),
                         evento.getDescripcion(),
                         evento.getColor(),
-                        evento.getUbicacion(),
+                        ubicacionDTO,
                         evento.getFechaInicio(),
                         evento.getHoraInicio(),
                         evento.getHorasDuracionEvento()
@@ -123,7 +182,7 @@ class Conversiones {
                         evento.getDescripcion(),
                         evento.getColor(),
                         evento.getDiasSemana(),
-                        evento.getUbicacion(),
+                        ubicacionDTO,
                         evento.getFechaInicio(),
                         evento.getFechaFin(),
                         evento.getHoraInicio(),
@@ -136,12 +195,16 @@ class Conversiones {
                         evento.getDescripcion(),
                         evento.getColor(),
                         evento.getDiasSemana(),
-                        evento.getUbicacion(),
+                        ubicacionDTO,
                         evento.getFechaInicio(),
                         evento.getFechaFin(),
                         evento.getHoraInicio(),
                         evento.getHorasDuracionEvento()
                 );
+        }
+        if(eventoConvertido!=null){
+            eventoConvertido.setId(evento.getId());
+            eventoConvertido.setMaestro(maestroDTO2);
         }
         return eventoConvertido;
     }
