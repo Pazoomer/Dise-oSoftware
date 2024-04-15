920340d85f7166cc749c51d88486ad92ccafcec9
luisa <moraLF20@outlook.com>
2024-04-14 21:57:38 -0700

    cambiando de paquete a persistencia exception

--- AulasPersistencia/src/main/java/excepciones/PersistenciaException.java
+++ AulasPersistencia/src/main/java/excepciones/PersistenciaException.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package DAO.exceptions;
+package excepciones;
 
 /**
  *
  * @author luiis
  */
-public class PersistenciaException extends Exception {
+public class PersistenciaException extends Exception{
 
     public PersistenciaException() {
     }
