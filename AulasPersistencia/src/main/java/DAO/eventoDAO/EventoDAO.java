caba1aac94992d13e7701d9f73b5351df65957b7
luisa <moraLF20@outlook.com>
2024-04-14 22:06:04 -0700

    Update EventoDAO.java

--- AulasPersistencia/src/main/java/DAO/eventoDAO/EventoDAO.java
+++ AulasPersistencia/src/main/java/DAO/eventoDAO/EventoDAO.java
@@ -9,6 +9,8 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 import entidades.EntidadEvento;
 import entidades.EntidadMaestro;
+import objetosNegocio.Evento;
+import objetosNegocio.Maestro;
 
 /**
  *
@@ -22,7 +24,7 @@ public class EventoDAO implements IEventoDAO{
         this.conexion=conexion;
     }
 
-    @Override
+    //@Override
     public EntidadEvento guardarEvento(EntidadEvento evento) {
         EntityManager entityManager = conexion.crearConexion();
         entityManager.getTransaction().begin();
@@ -31,43 +33,68 @@ public class EventoDAO implements IEventoDAO{
         entityManager.close();
         return evento;
     }
+//
+//   // @Override
+//    public EntidadEvento actualizarEvento(EntidadEvento evento) {
+//        EntityManager entityManager = conexion.crearConexion();
+//        entityManager.getTransaction().begin();
+//        EntidadEvento eventoActualizado =entityManager.merge(evento);
+//        entityManager.getTransaction().commit();
+//        entityManager.close();
+//        return eventoActualizado;
+//    }
+//
+//    @Override
+//    public boolean eliminarEvento(long eventoId) {
+//        EntityManager entityManager = conexion.crearConexion();
+//        entityManager.getTransaction().begin();
+//        EntidadEvento evento = entityManager.find(EntidadEvento.class, eventoId);
+//        entityManager.remove(evento);
+//        entityManager.getTransaction().commit();
+//        entityManager.close();
+//        return true;
+//    }
+//
+//    @Override
+//    public EntidadEvento obtenerEventoPorId(long eventoId) {
+//        EntityManager entityManager = conexion.crearConexion();
+//        EntidadEvento eventoConsultado=entityManager.find(EntidadEvento.class, eventoId);
+//        entityManager.close();
+//        return eventoConsultado;
+//    }
+//
+//    @Override
+//    public List<EntidadEvento> obtenerTodosLosEventosPorMaestro(EntidadMaestro maestro) {
+//        EntityManager entityManager = conexion.crearConexion();
+//        Query query = entityManager.createQuery("SELECT e FROM Evento e WHERE e.maestro = :maestro");
+//        query.setParameter("maestro", maestro);
+//        List<EntidadEvento> eventos=query.getResultList();
+//        entityManager.close();
+//        return eventos;
+//    }
 
     @Override
-    public EntidadEvento actualizarEvento(EntidadEvento evento) {
-        EntityManager entityManager = conexion.crearConexion();
-        entityManager.getTransaction().begin();
-        EntidadEvento eventoActualizado =entityManager.merge(evento);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        return eventoActualizado;
+    public Evento guardarEvento(Evento evento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public boolean eliminarEvento(long eventoId) {
-        EntityManager entityManager = conexion.crearConexion();
-        entityManager.getTransaction().begin();
-        EntidadEvento evento = entityManager.find(EntidadEvento.class, eventoId);
-        entityManager.remove(evento);
-        entityManager.getTransaction().commit();
-        entityManager.close();
-        return true;
+    public Evento actualizarEvento(Evento evento) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public EntidadEvento obtenerEventoPorId(long eventoId) {
-        EntityManager entityManager = conexion.crearConexion();
-        EntidadEvento eventoConsultado=entityManager.find(EntidadEvento.class, eventoId);
-        entityManager.close();
-        return eventoConsultado;
+    public List<Evento> obtenerTodosLosEventosPorMaestro(Maestro mstr) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
-    public List<EntidadEvento> obtenerTodosLosEventosPorMaestro(EntidadMaestro maestro) {
-        EntityManager entityManager = conexion.crearConexion();
-        Query query = entityManager.createQuery("SELECT e FROM Evento e WHERE e.maestro = :maestro");
-        query.setParameter("maestro", maestro);
-        List<EntidadEvento> eventos=query.getResultList();
-        entityManager.close();
-        return eventos;
+    public boolean eliminarEvento(long l) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Evento obtenerEventoPorId(long l) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
