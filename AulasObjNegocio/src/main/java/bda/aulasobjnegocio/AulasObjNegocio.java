e36e2514eff25143206d64a9ce17cd93c9841111
Luisa Morales <144541458+lumm20@users.noreply.github.com>
2024-04-28 02:01:34 -0700

    Update AulasObjNegocio.java

--- AulasObjNegocio/src/main/java/bda/aulasobjnegocio/AulasObjNegocio.java
+++ AulasObjNegocio/src/main/java/bda/aulasobjnegocio/AulasObjNegocio.java
@@ -5,6 +5,8 @@ package bda.aulasobjnegocio;
 import DTOS.campus.UbicacionDTO;
 import DTOS.maestro.MaestroEditableDTO;
 import excepciones.NegocioException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import objetosNegocio.Evento;
 import objetosNegocio.Maestro;
@@ -17,6 +19,35 @@ import objetosNegocio.Ubicacion;
 public class AulasObjNegocio {
 
     public static void main(String[] args) {
+        
+//        Calendar fechaBase1=Calendar.getInstance();
+//        fechaBase1.set(Calendar.MONTH, 3);
+//        fechaBase1.set(Calendar.DAY_OF_MONTH, 10);
+//        fechaBase1.set(Calendar.YEAR, 2023);
+//        Calendar fechaBase2=Calendar.getInstance();
+//        fechaBase2.set(Calendar.MONTH, 4);
+//        fechaBase2.set(Calendar.DAY_OF_MONTH, 20);
+//        fechaBase2.set(Calendar.YEAR, 2023);
+//        Calendar fechaBase3=Calendar.getInstance();
+//        fechaBase3.set(Calendar.HOUR_OF_DAY,10);
+//        fechaBase3.set(Calendar.MINUTE,0);
+//        
+//        Calendar fechaInicio =Calendar.getInstance();
+//        Date date=fechaBase1.getTime();
+//        fechaInicio.setTime(date);
+//        Calendar fechaFin=Calendar.getInstance();
+//        date=fechaBase2.getTime();
+//        fechaFin.setTime(date);
+//        Calendar horaInicio=Calendar.getInstance();
+//        date=fechaBase3.getTime();
+//        horaInicio.setTime(date);
+//        
+//        System.out.println("fecha inicio: "+fechaInicio.get(Calendar.DAY_OF_MONTH)+"/"+
+//                fechaInicio.get(Calendar.MONTH)+"/"+fechaInicio.get(Calendar.YEAR));
+//        System.out.println("fecha fin: "+fechaFin.get(Calendar.DAY_OF_MONTH)+"/"+
+//                fechaFin.get(Calendar.MONTH)+"/"+fechaFin.get(Calendar.YEAR));
+//        System.out.println("hora inicio: "+horaInicio.get(Calendar.HOUR_OF_DAY)+":"+
+//                horaInicio.get(Calendar.MINUTE));
         Maestro m=new Maestro();
         MaestroEditableDTO maestroDTO=new MaestroEditableDTO(1L);
         try{
