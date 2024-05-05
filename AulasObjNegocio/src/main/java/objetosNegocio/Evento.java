ffbb953838574b5bee702cc44182fbcfdc59673f
luisa <moraLF20@outlook.com>
2024-05-05 12:44:54 -0700

    agregando bo de evento

---
+++ AulasObjNegocio/src/main/java/objetosNegocio/Evento.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package objetosNegocio;
+
+import DTOS.evento.EventoConsultableDTO;
+import entidades.CrudCampus;
+import entidades.CrudEvento;
+import entidades.EntidadEvento;
+import entidades.EntidadUbicacion;
+import excepciones.NegocioException;
+import excepcioness.PersistenciaExceptionn;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author luiis
+ */
+public class Evento {
+    private final CrudEvento crudEvento;
+    private final Conversiones conversiones;
+    
+    public Evento() {
+        this.crudEvento=new CrudEvento();
+        this.conversiones=new Conversiones();
+    }
+    
+    public EventoConsultableDTO agregarEvento(EventoConsultableDTO evento)throws NegocioException{
+        CrudCampus crudC=new CrudCampus();
+        try{
+            EntidadUbicacion ubicacion=crudC.obtenerUbi(new EntidadUbicacion(evento.getUbicacion().getIdentificador()));
+            if(ubicacion!=null){
+                System.out.println(ubicacion);
+                EntidadEvento e=conversiones.toEventoBO(evento);
+                e.setUbicacion(ubicacion);
+                e=crudEvento.agregarEvento(e);
+                if(e!=null)return conversiones.toEventoDTO(e);
+                return null;
+            }throw new NegocioException("La ubicacion ingresada no existe");
+        }catch(PersistenciaExceptionn e){
+            throw new NegocioException(e.getMessage());
+        }
+    }
+    
+    public EventoConsultableDTO editarEvento(EventoConsultableDTO evento,List<String> camposModificados)throws NegocioException{
+        try{
+            EntidadEvento e=crudEvento.editarEvento(conversiones.toEventoBO(evento),camposModificados);
+            if(e!=null)return conversiones.toEventoDTO(e);
+            else return null;
+        }catch(PersistenciaExceptionn e){
+            throw new NegocioException(e.getMessage());
+        }
+    }
+    
+    public boolean eliminarEvento(EventoConsultableDTO evento)throws NegocioException{
+        try{
+            return crudEvento.eliminarEvento(conversiones.toEventoBO(evento));
+        }catch(PersistenciaExceptionn e){
+            throw new NegocioException(e.getMessage());
+        }
+    }
+    
+    public List<EventoConsultableDTO> obtenerEventos()throws NegocioException{
+        try{
+            List<EventoConsultableDTO> eventosDTO=new ArrayList<>();
+            List<EntidadEvento> eventos=crudEvento.obtenerEventos();
+            if(eventos!=null){
+                for (EntidadEvento ev : eventos) {
+                    eventosDTO.add(conversiones.toEventoDTO(ev));
+                }
+                return eventosDTO;
+            }
+            return null;
+        }catch(PersistenciaExceptionn e){
+            throw new NegocioException(e.getMessage());
+        }
+    }
+    
+    public EventoConsultableDTO obtenerEvento(EventoConsultableDTO evento)throws NegocioException{
+        try {
+            EntidadEvento eventoBuscado=new EntidadEvento();
+            eventoBuscado.setNombre(evento.getNombre());
+            EntidadEvento ev=crudEvento.obtenerEvento(eventoBuscado);
+            if(ev!=null)return conversiones.toEventoDTO(ev);
+            return null;
+        } catch (Exception e) {
+            throw new NegocioException(e.getMessage());
+        }
+    }
+}
