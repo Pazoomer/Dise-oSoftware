ae70ddb33a36586f50ae055bb50279f9429211bc
luisa <moraLF20@outlook.com>
2024-05-04 20:11:41 -0700

    modificando la clase conexion
    >la hice singleton
    >cambie la configuracion del codec y el clientSettings

--- AulasPersistencia/src/main/java/entidades/Conexion.java
+++ AulasPersistencia/src/main/java/entidades/Conexion.java
@@ -1,13 +1,11 @@
 package entidades;
 
-import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
 
@@ -17,34 +15,51 @@ import org.bson.codecs.pojo.PojoCodecProvider;
  */
 public class Conexion implements IConexion{
 
-    String cadenaConexion = "mongodb://127.0.0.1:27017";
-    String NombrebaseDatos = "Aulas";
+    private static MongoClient mongoClient = null;
+    private static final String URI = "mongodb://localhost:27017";
+    private static final String DB_NAME = "Aulas";
 
-    private final MongoClient cliente;
-    private final MongoDatabase baseDatos;
+//    private static final String cadenaConexion = "mongodb://localhost:27017";
+//    private static final String NombrebaseDatos = "Aulas";
+//
+//    private static MongoClient cliente=null;
 
-    protected Conexion() {
-        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
-                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(new ConnectionString(cadenaConexion))
-                .codecRegistry(pojoCodecRegistry)
-                .build();
-        
-        cliente = MongoClients.create(settings);
-
-        baseDatos = cliente.getDatabase(NombrebaseDatos);
-
+    private Conexion() {
     }
 
+    public static MongoDatabase getDatabasee(){
+        if (mongoClient == null) {
+            CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+                    CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+
+            MongoClientSettings clientSettings = MongoClientSettings.builder()
+                    .applyConnectionString(new com.mongodb.ConnectionString(URI))
+                    .codecRegistry(pojoCodecRegistry).build();
+
+            mongoClient = MongoClients.create(clientSettings);
+            return mongoClient.getDatabase(DB_NAME).withCodecRegistry(pojoCodecRegistry);
+        }
+        return mongoClient.getDatabase(DB_NAME);
+//        if (cliente == null) {
+//            CodecRegistry pojoCodecRegistry = CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+//                    CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+//
+//            MongoClientSettings settings = MongoClientSettings.builder()
+//                    .applyConnectionString(new com.mongodb.ConnectionString(cadenaConexion))
+//                    .codecRegistry(pojoCodecRegistry).build();
+//            
+//            cliente = MongoClients.create(settings);
+//            return cliente.getDatabase(NombrebaseDatos).withCodecRegistry(pojoCodecRegistry);
+//        }
+//        return cliente.getDatabase(NombrebaseDatos);
+    }
     /**
      *
      */
     @Override
     public void cerrarConexion() {
-        if (cliente != null) {
-            cliente.close();
+        if (mongoClient != null) {
+            mongoClient.close();
         }
 
     }
@@ -55,7 +70,7 @@ public class Conexion implements IConexion{
      */
     @Override
     public MongoCollection<EntidadMaestro> ConversionDocumentMaestro() {
-        return baseDatos.getCollection("Maestros", EntidadMaestro.class);
+        return getDatabasee().getCollection("Maestros", EntidadMaestro.class);
     }
 
     /**
@@ -64,12 +79,12 @@ public class Conexion implements IConexion{
      */
     @Override
     public MongoCollection<EntidadCampus> ConversionDocumentCampus() {
-        return baseDatos.getCollection("Campus", EntidadCampus.class);
+        return getDatabasee().getCollection("Campus", EntidadCampus.class);
     }
 
     @Override
     public MongoCollection<EntidadEvento> ConversionDocumentEvento() {
-        return baseDatos.getCollection("Eventos", EntidadEvento.class);
+        return getDatabasee().getCollection("Eventos", EntidadEvento.class);
     }
 
 }
